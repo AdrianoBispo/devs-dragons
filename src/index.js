360f2e2d071fa7cfb540e9b4cfbf1b5ad0ce571e
Adriano Bispo <drianvini10@gmail.com>
2025-02-05 15:57:51 -0300

    Arquivos renomeados e classe Mago criada.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import { Personagem } from "./modules/personagem.js"
-import { PersonagemView } from "./components/personagem-view.js";
+import { Personagem } from "./modules/Personagem.js"
+import { Mago } from "./modules/Mago.js";
+import { PersonagemView } from "./components/PersonagemView.js";
 
 const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago');
 const personagemJose = new Personagem('Jose', 3, 'Arqueiro');
+
+//console.log(new Mago('gelo', 7, 9))
 
 const personagens = [personagemPedrinho, personagemJose];
 
