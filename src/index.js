69dfbe97edb7048ea362b8c057dae541a9b0ceca
Adriano Bispo <drianvini10@gmail.com>
2025-02-05 19:17:33 -0300

    A classe Arqueiro foi criada a adicionada ao projeto.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { Personagem } from "./modules/Personagem.js"
 import { Mago } from "./modules/Mago.js";
+import { Arqueiro } from "./modules/Arqueiro.js";
 import { PersonagemView } from "./components/PersonagemView.js";
 
+const arqueiroBruno = new Arqueiro('Bruno', 7, 8)
 const magoAntonio = new Mago('Antonio', 4, 'Fogo', 4, 3);
 const magaJulia = new Mago('Julia', 8, 'Gelo', 7, 10);
 
-const personagens = [magoAntonio, magaJulia];
+const personagens = [magoAntonio, magaJulia, arqueiroBruno];
 
 new PersonagemView(personagens).render()
---
+++ src/modules/Arqueiro.js
@@ -0,0 +1,19 @@
+import { Personagem } from "./Personagem.js";
+
+export class Arqueiro extends Personagem {
+    destreza
+    tipo = 'Arqueiro'
+
+    constructor(nome, level, destreza) {
+        super(nome, level)
+        this.destreza = destreza;
+    }
+
+    obterInsignia() {
+        if (this.destreza >= 5) {
+            return 'Dominador de Flechas';
+        }
+        return super.obterInsignia();
+    }
+
+}
