681b29d9fd789a32c527c5fc08b658dae47b1018
Adriano Bispo <drianvini10@gmail.com>
2025-02-05 19:36:34 -0300

    Nova classe criada: ArqueiroMago. Essa classe é uma junção dos poderes de Arqueiro e Mago.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import { Personagem } from "./modules/Personagem.js"
 import { Mago } from "./modules/Mago.js";
 import { Arqueiro } from "./modules/Arqueiro.js";
 import { PersonagemView } from "./components/PersonagemView.js";
+import { ArqueiroMago } from "./modules/ArqueiroMago.js";
 
-const arqueiroBruno = new Arqueiro('Bruno', 7, 8)
 const magoAntonio = new Mago('Antonio', 4, 'Fogo', 4, 3);
 const magaJulia = new Mago('Julia', 8, 'Gelo', 7, 10);
+const arqueiroBruno = new Arqueiro('Bruno', 7, 8)
+const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
 
-const personagens = [magoAntonio, magaJulia, arqueiroBruno];
+const personagens = [magoAntonio, magaJulia, arqueiroBruno, arqueiroMagoChico];
 
 new PersonagemView(personagens).render()
